8b71c1210d5505b75caf77f374a295fc22787a56
abhuraya <abhuraya@gmail.com>
2024-12-28 22:34:10 -0500

    recvamp

---
+++ script.js
@@ -0,0 +1,6 @@
+let fruits = ['Apple', 'Apricot', 'Banana', 'Blackberry', 'Blueberry', 'Boysenberry', 'Canary Melon', 'Cantaloupe', 'Casaba Melon', 'Cherimoya', 'Cherry', 'Christmas Melon', 'Clementine', 'Coconut', 'Cranberry', 'Crenshaw Melon', 'Cucumber', 'Currant', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Goji Berry', 'Gooseberry', 'Grape', 'Grapefruit', 'Guava', 'Honeydew', 'Horned Melon', 'Huckleberry', 'Jackfruit', 'Jujube', 'Kiwi', 'Kumquat', 'Lemon', 'Lime', 'Loganberry', 'Lychee', 'Mandarin Orange', 'Mango', 'Minneola', 'Mulberry', 'Musk Melon', 'Nance', 'Nectarine', 'Blood Orange', 'Orange', 'Papaya', 'Passion Fruit', 'Peach', 'Pear', 'Persimmon', 'Pineapple', 'Plum', 'Pomegranate', 'Prickly (cactus) pear', 'Pomelo', 'Pulasan', 'Rambutan', 'Raspberry', 'Red Currant', 'Satsuma', 'Soutsop', 'Star Fruit', 'Strawberry', 'Tamarillo', 'Tangelo', 'Tangerine', 'Ugli fruit', 'Watermelon', 'White currant', 'White sapote', 'Yuzu', 'Ziziphus'];
+
+document.querySelector('button').addEventListener('click', function() {
+    let randomIndex = Math.floor(Math.random() * fruits.length);
+    document.querySelector('#display').textContent = fruits[randomIndex];
+});
